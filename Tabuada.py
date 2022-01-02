63f16d1dd16ee0fb5ce450b350f6a816537f9835
João Tobias da Silva Pontes <tobiaspontes@gmail.com>
2022-01-02 18:48:42 -0300

    Alteração Tabuada
    Ajustes no código.

---
+++ Tabuada.py
@@ -0,0 +1,11 @@
+# Tabuada em Python
+
+def tabuada(x):
+    for i in range(10):
+        print('{} x {} = {}'.format(x, (1 + 1), x * (i + 1)))
+    print()
+
+if __name__ == '__main__':
+    nro = int(input('Entre com um número: '))
+    print(f'\n\033[1;32mTabuada do {nro}'+'\n')
+    tabuada(nro)
