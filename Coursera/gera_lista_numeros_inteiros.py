4f32dfc26020dec5b814753c4eb76c2c899db712
João Tobias <tobiaspontes@gmail.com>
2022-07-11 19:32:37 -0300

    Ajuste na geração de lista de números

--- Coursera/gera_lista_numeros_inteiros.py
+++ Coursera/gera_lista_numeros_inteiros.py
@@ -1,4 +1,4 @@
-# esse código gera uma lista com números inteiros aleatórios entre 1 e 1000
+# esse código gera uma lista com números inteiros aleatórios
 
 import random
 
@@ -7,21 +7,33 @@ def mensagem_inicial():
     print(55 * '*')
     print(9 * '*', 'Lista de Números Inteiros Aleatória', 9 * '*')
     print('*', 51 * ' ', '*')
-    print('** Os números gerados estão no intervalo de 1 a 1000 **')
+    print('*   Informe quantidade, limites inferior e superior   *')
     print('*', 51 * ' ', '*')
     print(55*'*')
 
-def gera_numeros():
+def gera_numeros(n, limite_inferior, limite_superior):
     lista_numeros = []
-    n = int(input('\nQuantos números na lista?: '))
     i = 1
     while i <= n:
-        numero = random.randrange(1, 1001)
+        numero = random.randrange(limite_inferior, limite_superior + 1)
         lista_numeros.append(numero)
         i += 1
     return lista_numeros
 
+def verifica_erro(n, limite_inferior, limite_superior):
+    intervalo = (limite_superior + 1) - limite_inferior
+    if (intervalo <= 0):
+        print('Limite superior deve ser maior do que limite inferior!')
+        exit()
+    if (n > intervalo):
+        print('Quantidade de números solicitada é maior do que o intervalo!')
+        exit()
+
 if (__name__ == '__main__'):
     mensagem_inicial()
-    lista_numeros = gera_numeros()
+    n = int(input('\nQuantos números na lista? '))
+    limite_inferior = int(input('Qual o limite inferior? '))
+    limite_superior = int(input('Qual o limite superior? '))
+    verifica_erro(n, limite_inferior, limite_superior)
+    lista_numeros = gera_numeros(n, limite_inferior, limite_superior)
     print('\nA lista gerada é: {}\n'.format(lista_numeros))
