aeabb2da91f91540800b3567621b3cd55cb30e8d
João Tobias <tobiaspontes@gmail.com>
2022-07-11 16:37:10 -0300

    Atualização para gerar lista aleatória de números inteiros

--- Coursera/gera_lista_numeros_inteiros.py
+++ Coursera/gera_lista_numeros_inteiros.py
@@ -1,17 +1,27 @@
-# esse código pede ao usuário para digitar números inteiros e cria uma lista; digitar zero termina o processo
+# esse código gera uma lista com números inteiros aleatórios entre 1 e 1000
+
+import random
+
+def mensagem_inicial():
+    print()
+    print(55 * '*')
+    print(9 * '*', 'Lista de Números Inteiros Aleatória', 9 * '*')
+    print('*', 51 * ' ', '*')
+    print('** Os números gerados estão no intervalo de 1 a 1000 **')
+    print('*', 51 * ' ', '*')
+    print(55*'*')
 
 def gera_numeros():
-    terminou = False
     lista_numeros = []
-
-    while (not terminou):
-        n = int(input('Digite um número inteiro, ou zero para terminar: '))
-        if n == 0:
-            terminou = True
-        else:
-            lista_numeros.append(n)
+    n = int(input('\nQuantos números na lista?: '))
+    i = 1
+    while i <= n:
+        numero = random.randrange(1, 1001)
+        lista_numeros.append(numero)
+        i += 1
     return lista_numeros
 
 if (__name__ == '__main__'):
+    mensagem_inicial()
     lista_numeros = gera_numeros()
-    print(lista_numeros)
+    print('\nA lista gerada é: {}\n'.format(lista_numeros))
