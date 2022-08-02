0787c7354c32f01b1c0a972a493fd267d2aac49b
João Tobias <tobiaspontes@gmail.com>
2022-08-02 15:02:24 -0300

    Atualização de código

--- busca_no_google.py
+++ busca_no_google.py
@@ -1,19 +1,17 @@
 # Este código utiliza a biblioteca googlesearch para realizar uma pesquisa no google
 
-from googlesearch import search
+from googlesearch import Search
 
 # Texto da busca
 query = 'roger federer'
 
 # Faz a busca e cria lista com os links dos resultados
-result = list(
-    search(
-        query,
-        lang='pt-br',
-        num_results=5
-    )
-)
+# result = list(Search(query, lang='pt-br', num_results=5))
+resultado = Search(query, language='pt-br',number_of_results=10)
 
 # Mostra resultados
 print()
-print(result)
+tamanho = len(resultado.results)
+for i in range(tamanho):
+    print(resultado.results[i])
+    i += 1
